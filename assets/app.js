2e71ed98495a8718325d2e0b98caba101b600a6b
Stroupe79 <cstroupe79@gmail.com>
2019-03-29 13:25:45 -0500

    New buttons

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,49 @@
 //append new button to #gifDisplay on click 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-var search = $("#searchField")
-console.log(search.attr('value'))
+    var gifs = ["Cats", "Dogs", "Horses", "Ducks"];
 
-$("#searchField").keyup(function(event) {
-    if (event.keyCode === 13) {
-      console.log(search.attr('value'));
-      event.preventDefault();
-      searchGIF(search.attr('value'));
+    buttons();
+
+    function buttons() {
+        $("#gifDisplay").empty();
+        for (var i = 0; i < gifs.length; i++) {
+            let button = $("<button>")
+            button.addClass("btn btn-success m-5")
+            button.attr("val", gifs[i]);
+            button.text(gifs[i]);
+            $("#gifDisplay").append(button);
+
+        }
+
     }
-  });
 
-  function searchGIF(searchVal){
-      console.log(searchVal);
-  }
-  
+    $("#searchField").keyup(function (event) {
+        if (event.keyCode === 13) {
+            var search = $("#searchField").val();
+            searchGIF(search);
+            console.log(search)
+            event.preventDefault();
+            gifs.push(search);
+            search = "";
+            buttons();
+        }
+    });
+
+    function searchGIF(searchVal) {
+        console.log(searchVal);
+        let query = "https://api.giphy.com/v1/gifs/search?q=" + searchVal + "&api_key=va3byTEOOuISlLe0hh47DYH0psyejaCo&limit=8";
+        $.ajax({
+            url: query,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response.data[0].url)
+            $("#gifShow").append("<img src=" + response.data[0].url + " />")
+        });
+
+
+
+    }
+
 });
